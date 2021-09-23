7659de7aaf348a5a3498f4a861e73e352499a0a1
MKDLi <MKDLi@MPC>
2021-09-23 20:39:48 +0530

    Add project files.

---
+++ LHBOL/URLs.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LHBOL
+{
+    public class URLs
+    {
+        public int UrlId { get; set; }
+        public string UrlTitle { get; set; }
+        public string UrlName { get; set; }
+        public string UrlDescription { get; set; }
+
+    }
+}
---
+++ LHBOL/User.cs
@@ -0,0 +1,11 @@
+﻿using System;
+
+namespace LHBOL
+{
+    public class User
+    {
+        public int UserId { get; set; }
+        public string UserEmail { get; set; }
+        public string Password { get; set; }
+    }
+}
